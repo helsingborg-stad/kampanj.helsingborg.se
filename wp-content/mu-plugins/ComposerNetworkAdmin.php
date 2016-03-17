a7fbb00b92fb1e0740b42bf3567e1c23a41742dd
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-03-17 13:53:27 +0100

    Admin url fixes.

--- wp-content/mu-plugins/ComposerNetworkAdmin.php
+++ wp-content/mu-plugins/ComposerNetworkAdmin.php
@@ -13,7 +13,17 @@ class ComposerNetworkAdmin
 {
     public function __construct()
     {
-        add_filter('network_admin_url', array($this, 'sanitizeNetworkAdminUrl'), 50);
+        add_filter('network_admin_url', array($this, 'sanitizeNetworkAdminUrl'), 50, 2);
+        add_filter('admin_url', array($this, 'sanitizeAdminUrl'), 50, 3);
+    }
+
+    public function sanitizeAdminUrl($url, $path, $blog_id)
+    {
+        if (strpos($url, '/wp/wp-admin') === false && !strpos($url, '/network')) {
+            return str_replace('/wp-admin/', '/wp/wp-admin/', $url);
+        } else {
+            return $url;
+        }
     }
 
     public function sanitizeNetworkAdminUrl($url, $path)
