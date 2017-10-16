7c40fbef2fd4690c431a7b68bfa1ed26e7feecd2
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-16 08:49:22 +0200

    Updated cookie config to support domains contianing helsingborg.se
    But not a member of the doimain. For example: pedagogsajten.familjenhelsingborg.se or familjehusethelsingborg.se

--- config/cookie.php
+++ config/cookie.php
@@ -7,7 +7,7 @@
 
 if (!isset($_SERVER['HTTP_HOST']) || (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], "helsingborg.dev") !== false)) {
     define('COOKIE_DOMAIN', ".helsingborg.dev");
-} elseif (strpos($_SERVER['HTTP_HOST'], "helsingborg.se") !== false) {
+} elseif (strpos($_SERVER['HTTP_HOST'], ".helsingborg.se") !== false) {
     define('COOKIE_DOMAIN', ".helsingborg.se");
 } else {
     define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
