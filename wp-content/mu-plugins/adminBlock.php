fecaeffbe3d49a63c51b08f4055c647fe191326d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2018-10-22 08:48:38 +0200

    Alloow superadmins in admin block

--- wp-content/mu-plugins/adminBlock.php
+++ wp-content/mu-plugins/adminBlock.php
@@ -18,7 +18,7 @@ class AdminBlock
 
     public function blockAdmin($sitesItems = array())
     {
-        if (is_multisite() &&  in_array(get_current_blog_id(), array(29))) {
+        if (is_multisite() && in_array(get_current_blog_id(), array(29)) && !is_super_admin()) {
             wp_die("<h1/>Webbplatsen tillfälligt otillgänlig</h1> <p>Den här webbplatsen flyttas till en ny server. <ul><li>Tid: Flytten planeras att ta ett par timmar.</li><li>Redakötrer: Kan inte redigera innehåll under tiden.</li><li>Besökare: Ingen påverkan, webbplatsen tillgänglig under hela flytten.</li></ul></p>", "Webbplatsen tillfälligt otillgänlig. ");
         }
     }
